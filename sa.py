b14b540746ff3b8d7c59d81402d40169231ca51a
Phil Stubbings <phil@parasec.net>
2017-09-25 18:03:10 +0100

    simulating annealing

--- sa.py
+++ sa.py
@@ -49,6 +49,7 @@ def eval_candidate(grid, adj):
     return score
 
 
+
 conf = load_conf('geojson/counties.geojson', 'Name')
 regions = conf['regions']
 neighbours = conf['neighbours']
@@ -57,8 +58,35 @@ rows = cols = 10
 grid = random_grid(regions, rows, cols)
 adj_matrix = adjacency_matrix(neighbours, regions)
 
-score = eval_candidate(grid, adj_matrix)
+last_max = max_score = eval_candidate(grid, adj_matrix)
 
-print(grid)
-print(adj_matrix)
-print(score)
+t = 1.0
+a = 0.999999
+#t=0.001
+#a=1
+
+while True:
+    if t > 0.000001:
+        t = t*a
+
+    i1 = random.randint(0, rows - 1)
+    j1 = random.randint(0, cols - 1)
+    i2 = random.randint(0, rows - 1)
+    j2 = random.randint(0, cols - 1)
+       
+    v = grid[i1][j1]
+    grid[i1][j1] = grid[i2][j2]
+    grid[i2][j2] = v
+
+    score = eval_candidate(grid, adj_matrix)
+
+    if score > max_score:
+        last_max = max_score = score
+        print(t, score)
+    elif score > last_max or random.random() <= t:
+        last_max = score
+    else:
+        grid[i2][j2] = grid[i1][j1]
+        grid[i1][j1] = v
+ 
+
